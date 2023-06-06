39c9fabc919c0ed4631208983f90b894647dda03
rezarex <marvin.wekesa@gmail.com>
2023-06-06 11:11:04 +0300

    pong game

---
+++ pong.py
@@ -0,0 +1,28 @@
+'''
+learning by creating games...
+simple python pingpong game
+
+'''
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("PingPong by @wekessah")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+#main gae loop
+while True:
+    wn.update()
+
+
+#paddle A
+
+
+#paddle B
+
+
+#ball
